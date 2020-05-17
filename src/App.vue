61d2c8ce39be2ae598f999e9ce5ba3cec7606924
Gabriel Valderramos <gabrielvalderramos@gmail.com>
2020-05-17 17:51:31 -0300

    main component changes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Products
+      msg="test"
+      imgUrl="https://static.netshoes.com.br/produtos/tenis-nike-court-lite-2-masculino/06/HZM-1864-006/HZM-1864-006_zoom1.jpg?ts=1583774895&ims=544x"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from "./components/HelloWorld.vue";
+import Products from "./components/Products.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    //HelloWorld
+    Products
   }
-}
+};
 </script>
 
 <style>
