ac3773c0ce41e985bc9bae79d36146815ff2700a
Gabriel Valderramos <gabrielvalderramos@gmail.com>
2020-05-17 17:50:58 -0300

    add product component

---
+++ src/components/Products.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-products-class">
+    <div class="swap-products">
+      <div class="products-container">
+        <img :src="imgUrl" v-bind:alt="imgUrl" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Products",
+  props: {
+    msg: String,
+    imgUrl: String
+  }
+};
+</script>
+
+<style scoped>
+</style>
